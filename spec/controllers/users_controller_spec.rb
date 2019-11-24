2f16be350999a97986e1f27da3221efb5bee8368
EC2 Default User <ec2-user@ip-172-31-18-221.us-east-2.compute.internal>
2019-11-24 14:44:34 +0000

    ユーザーコントローラーのテスト追加

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,19 +17,21 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-  describe 'GET #create' do
-    before { get :create }
-    
-    it 'レスポンスコードが200であること' do
-      expect(response).to have_http_status(:ok)
-    end
-    
-    it 'newテンプレートをレンダリングすること' do
-      expect(response).to render_template :new
-    end
-
-    it '新しいuserオブジェクトがビューに渡されること' do
-      expect(assigns(:user)).to be_a_new User
+  describe 'POST #create' do
+    context '正しいユーザー情報が渡ってきた場合' do
+      let(:params) do
+        { user: {
+            name: 'ゲストユーザー',
+            email: 'guest@guest.com',
+            password: 'password12',
+            paddword_confirmation: 'password12',
+          }
+        }
+      end
+      
+      it 'ユーザーが一人増えていること' do
+        expect { post :create, params: params }.to change(User, :count).by(1)
+      end
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # describe '#age' do
-  #   context '20年前の生年月日の場合' do
-  #     let(:user) { User.new(birthday: Time.zone.now - 20.years) }
-      
-  #     it '年齢が20歳であること' do
-  #       expect(user.age).to eq 20
-  #     end
-  #   end
-  # end
-  
+
   context 'ユーザー登録' do
     it 'ユーザー名とメールアドレス、passwordが登録に必要' do
       user = User.create(
